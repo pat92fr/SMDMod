1318ba9ad4312bcd2cf335e42a2ecd4e20e2692e
BenjDC <ben.de.coninck@gmail.com>
2021-02-06 22:55:42 +0100

    test avec M5Stack fire

--- SMDFireTest/include/sm_protocol.h
+++ SMDFireTest/include/sm_protocol.h
@@ -5,7 +5,122 @@
 #include <M5Stack.h>
 #define GPIO_PIN26 26
 #define GPIO_PIN36 36
-HardwareSerial MySerial(2);
+
+// REGISTER ADDRESS ///////////////////////////////////////////////////////////
+
+// EEPROM
+#define REG_MODEL_NUMBER_L 			0x00
+#define REG_MODEL_NUMBER_H 			0x01
+#define REG_VERSION		 			0x02
+#define REG_ID             			0x03
+#define REG_BAUD_RATE      			0x04
+#define REG_RETURN_DELAY   			0x05
+
+#define REG_RESERVED_EEPROM_1		0x10 // unused
+#define REG_RESERVED_EEPROM_2		0x11 // unused
+#define REG_RESERVED_EEPROM_3		0x12 // unused
+#define REG_RESERVED_EEPROM_4		0x13 // unused
+#define REG_MAX_VELOCITY_DPS_L		0x14
+#define REG_MAX_VELOCITY_DPS_H		0x15
+#define REG_MAX_ACCELERATION_DPSS_L	0x16
+#define REG_MAX_ACCELERATION_DPSS_H	0x17
+#define REG_MAX_CURRENT_MA_L		0x18
+#define REG_MAX_CURRENT_MA_H		0x19
+#define REG_MAX_PWM_100_L			0x1A
+#define REG_MAX_PWM_100_H			0x1B
+#define REG_TEMPERATURE_LIMIT		0x1C // no sensor
+#define REG_LOW_VOLTAGE_LIMIT		0x1D
+#define REG_HIGH_VOLTAGE_LIMIT		0x1E
+
+#define REG_MOVING_THRESHOLD_DPS	0x1F
+#define REG_STATUS_RETURN_LVL		0x20
+#define REG_ALARM_LED				0x21
+#define REG_ALARM_SHUTDOWN			0x22
+
+#define REG_CPR_L					0x23
+#define REG_CPR_H					0x24
+#define REG_REDUCER_L				0x25
+#define REG_REDUCER_H				0x26
+#define REG_RESERVED_EEPROM_5		0x27 // unused
+#define REG_INV_ROTATION_MOTOR		0x28
+#define REG_INV_ROTATION_SENSOR 	0x29
+
+#define REG_PID_VELOCITY_KP_L		0x2A
+#define REG_PID_VELOCITY_KP_H		0x2B
+#define REG_PID_VELOCITY_KI_L		0x2C
+#define REG_PID_VELOCITY_KI_H		0x2D
+#define REG_PID_VELOCITY_KD_L		0x2E
+#define REG_PID_VELOCITY_KD_H		0x2F
+#define REG_PID_VELOCITY_KFF_L		0x30
+#define REG_PID_VELOCITY_KFF_H		0x31
+#define REG_PID_ACCELERATION_KFF_L	0x32
+#define REG_PID_ACCELERATION_KFF_H	0x33
+#define REG_PID_CURRENT_KP_L		0x34
+#define REG_PID_CURRENT_KP_H		0x35
+#define REG_PID_CURRENT_KI_L		0x36
+#define REG_PID_CURRENT_KI_H		0x37
+#define REG_PID_CURRENT_KFF_L		0x38
+#define REG_PID_CURRENT_KFF_H		0x39
+
+#define REG_CAL_CURRENT_SENSE_A_L	0x3A
+#define REG_CAL_CURRENT_SENSE_A_H	0x3B
+#define REG_CAL_VOLTAGE_SENSE_L		0x3C
+#define REG_CAL_VOLTAGE_SENSE_H		0x3D
+
+// RAM
+#define REG_TORQUE_ENABLE  					0x40
+#define REG_LED			  					0x41
+#define REG_CONTROL_MODE					0x42
+#define REG_GOAL_ACCELERATION_DPSS_L		0x43
+#define REG_GOAL_ACCELERATION_DPSS_H		0x44
+#define REG_GOAL_VELOCITY_DPS_L				0x45
+#define REG_GOAL_VELOCITY_DPS_H				0x46
+#define REG_GOAL_CURRENT_MA_L				0x47
+#define REG_GOAL_CURRENT_MA_H				0x48
+#define REG_GOAL_PWM_100_L					0x49
+#define REG_GOAL_PWM_100_H					0x4A
+#define REG_RESERVED_RAM_1					0x4B // unused
+#define REG_RESERVED_RAM_2					0x4C // unused
+#define REG_PRESENT_ACCELERATION_DPSS_L		0x4D
+#define REG_PRESENT_ACCELERATION_DPSS_H		0x4E
+#define REG_PRESENT_VELOCITY_DPS_L			0x4F
+#define REG_PRESENT_VELOCITY_DPS_H			0x50
+#define REG_PRESENT_CURRENT_MA_L			0x51
+#define REG_PRESENT_CURRENT_MA_H			0x52
+#define REG_PRESENT_VOLTAGE					0x53
+#define REG_PRESENT_TEMPERATURE 			0x54 // no sensor
+#define REG_MOVING			 				0x55
+// DEBUG RAM
+#define REG_SETPOINT_ACCELERATION_DPSS_L	0x56
+#define REG_SETPOINT_ACCELERATION_DPSS_H	0x57
+#define REG_SETPOINT_VELOCITY_DPS_L			0x58
+#define REG_SETPOINT_VELOCITY_DPS_H			0x59
+#define REG_SETPOINT_CURRENT_MA_L			0x5A
+#define REG_SETPOINT_CURRENT_MA_H			0x5B
+#define REG_SETPOINT_PWM_100_L				0x5C
+#define REG_SETPOINT_PWM_100_H				0x5D
+#define REG_MOTOR_CURRENT_INPUT_ADC_L 		0x5E
+#define REG_MOTOR_CURRENT_INPUT_ADC_H		0x5F
+#define REG_DEBUG_RAM_RESERVED_1   			0x60 // unused
+#define REG_DEBUG_RAM_RESERVED_2 			0x61 // unused
+#define REG_ENCODER_ABSOLUTE_L				0x62
+#define REG_ENCODER_ABSOLUTE_H				0x63
+#define REG_VOLTAGE_INPUT_ADC_L				0x64
+#define REG_VOLTAGE_INPUT_ADC_H				0x65
+// SW & HW ERROR
+#define REG_PROTOCOL_CRC_FAIL 				0x80
+#define REG_HARDWARE_ERROR_STATUS 			0x81
+// End
+#define REG_MAX             				0x82
+
+// REGISTER CONTROL MODE VALUES //////////////////////////////////////////////////////
+
+#define REG_CONTROL_MODE_OFF 0
+#define REG_CONTROL_MODE_ACCELERATION_PROFIL_VELOCITY_TORQUE 1
+#define REG_CONTROL_MODE_VELOCITY_TORQUE 2
+#define REG_CONTROL_MODE_CURRENT 3
+#define REG_CONTROL_MODE_PWM 4
+
 
 // Buffers
 #define TX_BUFFER_SIZE 256
@@ -55,9 +170,7 @@ HardwareSerial MySerial(2);
 class sm_protocol
 {
   
-public:
-
-  
+public:  
   sm_protocol(uint32_t baud):
     verbose(0),
     rx_packet_position(0),
@@ -67,7 +180,8 @@ public:
     rx_packet_instruction(0),
     rx_packet_error(0),
     rx_packet_crc(0),
-    rx_timeout_us(10000)
+    rx_timeout_us(10000),
+    MySerial(2)
     {
         // MEGA 16MHz:
         //  baud = 115200 OK 1.4ms reply
@@ -113,10 +227,13 @@ public:
   int readWordCommand(uint8_t id, uint16_t address, uint16_t length, uint16_t * data);
   int writeByteCommand(uint8_t id, uint16_t address, uint16_t length, uint8_t const * data);
   int writeWordCommand(uint8_t id, uint16_t address, uint16_t length, uint16_t const * data);  
+  void syncWriteByteCommand(uint16_t address, uint8_t number_of_ids, sync_byte_data const *data);
+  int syncWriteWordCommand(uint16_t address, uint8_t number_of_ids, sync_word_data const *data);
   int factoryResetCommand(uint8_t id);
   int rebootCommand(uint8_t id);
 
     // Utility SMS commands
+    /*
   void syncWriteByteCommand(uint16_t address, uint8_t number_of_ids, sync_byte_data const * data);
   int syncWriteWordCommand(uint16_t address, uint8_t number_of_ids, sync_word_data const * data);
   int enableTorque(uint8_t id);
@@ -133,6 +250,8 @@ public:
   float getPosition(uint8_t id, int * error);
   float getVelocity(uint8_t id, int * error);
   float getCurrent(uint8_t id, int * error);
+
+  */
     
 private:
 
@@ -148,6 +267,7 @@ private:
   uint8_t rx_packet_error;
   uint16_t rx_packet_crc;  
   unsigned long rx_timeout_us;
+  HardwareSerial MySerial;
 
   uint16_t updateCRC(uint16_t crc_accum, uint8_t *data_blk_ptr, uint16_t data_blk_size);  
   void serialFlush();
--- SMDFireTest/src/main.cpp
+++ SMDFireTest/src/main.cpp
@@ -1,95 +1,67 @@
 #include <M5Stack.h>
 #include "sm_protocol.h"
-sm_protocol servo(500000); 
+sm_protocol servo(500000);
 int error = 0;
 unsigned int counter = 0;
 float velocity = 50;
 
-void setup() {
+uint8_t byte_data = 0;
+uint16_t short_data = 0;
+uint16_t target_pwm = 0;
+uint16_t encoder_count = 0;
+uint16_t current_speed = 0;
+
+void setup()
+{
+
   M5.begin();
   M5.Power.begin();
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextColor(GREEN , BLACK);
-  M5.Lcd.setTextSize(5); 
+  M5.Lcd.setTextColor(GREEN, BLACK);
+  M5.Lcd.setTextSize(2);
 
-  // enable torque servo ID=1
-  error = servo.enableTorque(1);
-  if(error !=0)
+
+    // set PWM control mode
+  byte_data = REG_CONTROL_MODE_PWM;
+  error = servo.writeByteCommand(1, REG_CONTROL_MODE, 1, &byte_data);
+  if (error != 0)
   {
-    Serial.print("rx_packet_error:");
-    Serial.println(error);
+    M5.Lcd.setCursor(0, 30);
+    M5.Lcd.printf("control mode error : %i", error);
   }
-  // switch to "velocity-profil current based position control" operating mode
-  error = servo.setMode(1,2);
-  if(error !=0)
-  {
-    Serial.print("rx_packet_error:");
-    Serial.println(error);
-  }
-  // set position to 90°, limit velocity by 50 dps, motor current by 500mA and PWM ratio do 99% maximum
-  error = servo.setPositionVelocityCurrentPWM(1,90,50,500,99);
-  if(error !=0)
-  {
-    Serial.print("rx_packet_error:");
-    Serial.println(error);
-  }
-  delay(1000);
+
+
 }
 
-void loop() {
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.printf("Velocity:");
-  M5.Lcd.setCursor(0, 50);
-  M5.Lcd.printf("%.0f dps  ", velocity);
-  delay(1000);
+void loop()
+{
 
-  error = servo.setVelocityLimit(1,velocity);
-  if(error !=0)
+  M5.update();
+  if (M5.BtnB.wasPressed())
   {
-    Serial.print("rx_packet_error:");
-    Serial.println(error);
-  }  
-  error = servo.setPosition(1,150);
-  if(error !=0)
-  {
-    Serial.print("rx_packet_error:");
-    Serial.println(error);
-  }  
-  delay(1200);
-  error = servo.setPosition(1,90);
-  if(error !=0)
-  {
-    Serial.print("rx_packet_error:");
-    Serial.println(error);
-  }  
-  delay(1200);
-  error = servo.setPosition(1,30);
-  if(error !=0)
-  {
-    Serial.print("rx_packet_error:");
-    Serial.println(error);
-  }  
-  delay(1200);
-  error = servo.setPosition(1,90);
-  if(error !=0)
-  {
-    Serial.print("rx_packet_error:");
-    Serial.println(error);
-  }  
-  delay(1200);
-
- 
-  ++counter;
-  velocity = 2*velocity;
-  if(counter>=5)
-  {
-    error = servo.disableTorque(1);
-    if(error !=0)
-    {
-      Serial.print("rx_packet_error:");
-      Serial.println(error);
-    }
-    M5.Power.powerOFF();
+    target_pwm += 20;
   }
-  delay(100);
+
+  if (M5.BtnC.wasPressed())
+  {
+    target_pwm -= 20;
+  }
+
+  // set PWM speed
+  short_data = 0;
+  error = servo.writeWordCommand(1, REG_GOAL_PWM_100_L, 1, &target_pwm);
+
+  error = servo.readWordCommand(1, REG_ENCODER_ABSOLUTE_L, 1, &encoder_count);
+
+  error = servo.readWordCommand(1, REG_PRESENT_VELOCITY_DPS_L, 1, &current_speed);
+    
+
+
+    
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.printf("PWM target : %i            ", target_pwm);
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.printf("encoder count : %i         ", encoder_count);
+  M5.Lcd.setCursor(0, 60);
+  M5.Lcd.printf("current dps  : %i          ", current_speed);
 }
