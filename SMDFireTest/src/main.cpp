71ede95c4dc12ae1754baf30dcb504d9322d808e
BenjDC <ben.de.coninck@gmail.com>
2021-02-07 23:08:37 +0100

    PWM test + ROS connection

--- SMDFireTest/src/main.cpp
+++ SMDFireTest/src/main.cpp
@@ -1,36 +1,113 @@
 #include <M5Stack.h>
+
+#include <ros.h>
+#include "std_msgs/Int16.h"
+#include <WiFi.h>
+
 #include "sm_protocol.h"
+
+//ros handle object
+ros::NodeHandle nh;
+// std_msgs::Int16 Kp;
+// std_msgs::Int16 Ki;
+// std_msgs::Int16 Kd;
+// std_msgs::Int16 Kff;
+
+
+
+
+
+
+// Set the rosserial socket server IP address
+IPAddress server(192, 168, 0, 30);
+// Set the rosserial socket server port
+const uint16_t serverPort = 11411;
+
 sm_protocol servo(500000);
 int error = 0;
 unsigned int counter = 0;
 float velocity = 50;
 
 uint8_t byte_data = 0;
-uint16_t short_data = 0;
 uint16_t target_pwm = 0;
 uint16_t encoder_count = 0;
 uint16_t current_speed = 0;
+uint16_t current_voltage = 0;
+uint16_t present_current = 0;
+
+uint16_t pid_Kp = 0;
+uint16_t pid_Ki = 0;
+uint16_t pid_Kd = 0;
+uint16_t pid_Kff = 0;
+
+
+void KpCallback(const std_msgs::Int16 &kp)
+{
+  nh.loginfo("Kp modification ! ");
+  pid_Kp = kp.data;
+}
+
+
+
+ros::Subscriber<std_msgs::Int16> sub_Kp("Kp", &KpCallback);
+// ros::Suscriber<std_msgs::Int16> sub_Ki("Ki", &pid);
+// ros::Suscriber<std_msgs::Int16> sub_Kd("Kd", &pid);
+// ros::Suscriber<std_msgs::Int16> sub_Kff("Kff", &pid);
 
 void setup()
 {
 
   M5.begin();
   M5.Power.begin();
+
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextColor(GREEN, BLACK);
   M5.Lcd.setTextSize(2);
 
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.print("WiFi...");
 
-    // set PWM control mode
-  byte_data = REG_CONTROL_MODE_PWM;
-  error = servo.writeByteCommand(1, REG_CONTROL_MODE, 1, &byte_data);
-  if (error != 0)
+  const char *ssid = "SFR-5718";
+  const char *password = "2HI5D6N3NX3UTL56TTHL";
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
   {
-    M5.Lcd.setCursor(0, 30);
-    M5.Lcd.printf("control mode error : %i", error);
+    delay(500);
+    M5.Lcd.print(".");
+  }
+  M5.Lcd.print("OK !");
+
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.print("IP address: ");
+  M5.Lcd.print(WiFi.localIP());
+
+  nh.getHardware()->setConnection(server, serverPort);
+
+  nh.initNode();
+
+  nh.subscribe(sub_Kp);
+
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.print("ROSSerial...");
+
+  while (!nh.connected())
+  {
+    nh.spinOnce();
+    delay(100);
   }
 
+  M5.Lcd.print("OK !");
 
+  delay(1000);
+
+  M5.Lcd.fillScreen(BLACK);
+
+  // set PWM control mode
+  byte_data = 1;
+  error = servo.writeByteCommand(1, REG_TORQUE_ENABLE, 1, &byte_data);
+  byte_data = REG_CONTROL_MODE_PWM;
+  error = servo.writeByteCommand(1, REG_CONTROL_MODE, 1, &byte_data);
 }
 
 void loop()
@@ -48,20 +125,28 @@ void loop()
   }
 
   // set PWM speed
-  short_data = 0;
   error = servo.writeWordCommand(1, REG_GOAL_PWM_100_L, 1, &target_pwm);
 
   error = servo.readWordCommand(1, REG_ENCODER_ABSOLUTE_L, 1, &encoder_count);
 
   error = servo.readWordCommand(1, REG_PRESENT_VELOCITY_DPS_L, 1, &current_speed);
-    
 
+  error = servo.readWordCommand(1, REG_VOLTAGE_INPUT_ADC_L, 1, &current_voltage);
 
-    
+  error = servo.readWordCommand(1, REG_PRESENT_CURRENT_MA_L, 1, &present_current);
+
   M5.Lcd.setCursor(0, 20);
-  M5.Lcd.printf("PWM target : %i            ", target_pwm);
+  M5.Lcd.printf("target PWM : %i            ", (int16_t)target_pwm);
   M5.Lcd.setCursor(0, 40);
-  M5.Lcd.printf("encoder count : %i         ", encoder_count);
+  M5.Lcd.printf("present speed  : %irpm          ", (int16_t)(current_speed)*60 / 360);
   M5.Lcd.setCursor(0, 60);
-  M5.Lcd.printf("current dps  : %i          ", current_speed);
+  M5.Lcd.printf("present current  : %imA         ", (int16_t)(present_current));
+  M5.Lcd.setCursor(0, 80);
+  M5.Lcd.printf("PID  : KP %i Ki %i Kd %i Kff %i     ", pid_Kp, pid_Ki, pid_Kd, pid_Kff);
+  
+
+  M5.Lcd.setCursor(0, 200);
+  M5.Lcd.printf("battery voltage  : %i          ", (int16_t)current_voltage);
+
+  delay(100);
 }
